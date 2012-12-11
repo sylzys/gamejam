7d3fb5fcc852d34669d0863606760ac6efda80cb
Syl Zys <sylzys@gmail.com>
2012-12-11 10:59:17 +0100

    gestion jetpack

--- js/main.js
+++ js/main.js
@@ -4,6 +4,7 @@ var bg;
 var bgSrc = new Image();
 var gfxLoaded = 0; 
 var scientist;
+var sc;
 var speed = 8;  
 var moveLeft = false;  
 var moveRight = false;  
@@ -13,10 +14,8 @@ var BULLET_SPEED = 15;
 var SCREEN_WIDTH = 640;
 var SCREEN_HEIGHT = 480;
 var bullets = new Array();
-
+var score;
 function init() {
-
-
 	canvas = document.getElementById("canvas");
 	stage = new Stage(canvas);
 //KEYBOARD
@@ -31,13 +30,26 @@ bgSrc.onload = loadGfx;
 //SCIENTIST
 scImg = new Image();  
 scImg.onload = onScLoaded;  
-scImg.src  = "images/scientist.jpg";  
+scImg.src  = "images/scientist.png";  
+sc = new Bitmap(scImg);
+
+//CONTAINER
+icon = new Container();
+	icon.x = 10;
+	icon.y = 460;
+	stage.addChildAt(icon, 0);
+score = new Text("Score: 0", "20px Arial", "orange");
+//score.color("#ff0000");
+icon.Zindex=-1000;
+icon.addChild(score);
+stage.update();
 
 Ticker.setFPS(30);
 Ticker.addListener(window);
-tick();
+//tick();
 document.onkeydown = onKeyDown;
 document.onkeyup = onKeyUp;
+
 }
 
 function loadGfx(e)
@@ -58,18 +70,18 @@ function buildInterface(bmp)
 }
 
 function onScLoaded() {
-	sc = new Bitmap(scImg);
+	
 	sc.regX = sc.image.width * 0.5; 
 	sc.regY = sc.image.height * 0.5;
 	sc.x = 50; //placement en X
-	sc.y = 340; //placement en Y
+	sc.y = SCREEN_HEIGHT - 100; //placement en Y
 	buildInterface(sc);
 }
 
 function onKeyDown(e) {       
-    if(!e){ var e = window.event; }  
-              console.log(e.keyCode);
-    switch(e.keyCode) {  
+	if(!e){ var e = window.event; }  
+	console.log(e.keyCode);
+	switch(e.keyCode) {  
         // left  
         case 37: moveLeft = true; moveRight = false;      
         break;                    
@@ -87,11 +99,11 @@ function onKeyDown(e) {
         break;  
     }  
 }  
-          
+
 function onKeyUp(e) {         
-     if(!e){ var e = window.event; }  
-              console.log(e.keyCode);
-     switch(e.keyCode) {  
+	if(!e){ var e = window.event; }  
+	console.log(e.keyCode);
+	switch(e.keyCode) {  
         // left  
         case 37: moveLeft = false;   
         break;                    
@@ -108,29 +120,29 @@ function onKeyUp(e) {
 }  
 
 function checkMovement() {  
-    if(moveLeft)  
-    {  
-        sc.x -= speed;  
-        if(sc.x < 10)
-            sc.x = 10;  
-    }  
-    else if(moveRight)  
-    {  
-        sc.x += speed;  
-        if(sc.x > 630)  
-            sc.x = 630;  
-    }  
-                  
-    if(moveUp)  
-    {  
-        if(sc.y - speed > 24)  
-            sc.y -= speed * 2;  
-    }  
-    else if(moveDown)  
-    {  
-        if(sc.y + speed < 460)  
-            sc.y += speed;  
-    }  
+	if(moveLeft)  
+	{  
+		sc.x -= speed;  
+		if(sc.x < 10)
+			sc.x = 10;  
+	}  
+	else if(moveRight)  
+	{  
+		sc.x += speed;  
+		if(sc.x > SCREEN_WIDTH - 10)  
+			sc.x = SCREEN_WIDTH - 10;  
+	}  
+
+	if(moveUp)  
+	{  
+		if(sc.y - speed > 24)  
+			sc.y -= speed * 2;  
+	}  
+	else if(moveDown)  
+	{  
+		if(sc.y + speed < SCREEN_HEIGHT - 100)  
+			sc.y += speed;  
+	}  
 }  
 
 function fire(){
@@ -154,14 +166,21 @@ function updateBullets() {
 	for (i=0; i < limit; i++)
 	{
 		bullets[i].x += BULLET_SPEED;
-		if (bullets[i].x > 640){
+		if (bullets[i].x > SCREEN_WIDTH - 20){
 			stage.removeChild(bullets[i]);
 			bullets.splice(i, 1);
 		}
 	}
 }
+
+function updateScientist() {
+	if(sc.y + speed / 4 < SCREEN_HEIGHT - 100)  
+		sc.y += speed / 4;  
+}
+
 function tick() {  
-   checkMovement(); 
-   updateBullets(); 
-    stage.update();  
+	checkMovement(); 
+	updateScientist();
+	updateBullets(); 
+	stage.update();  
 } 
